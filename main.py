7cf818c2120a6bb6b9aa3fc97a18b5f45913e0e5
mz038197 <mz038197@gmail.com>
2026-04-22 15:49:46 +0800

    feat(main): 新增法鬥超人代理人基礎結構

---
+++ main.py
@@ -0,0 +1,17 @@
+import os
+from langchain_openai import ChatOpenAI
+
+from dotenv import load_dotenv
+load_dotenv()
+
+
+def main():
+    agent_name = "法鬥超人"
+    print(agent_name)
+
+
+if __name__ == "__main__":
+    main()
+
+
+
